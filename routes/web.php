5ec6856d66442df06d277f0f9735906075def5a9
NoahGoethals <noah.goethals@student.ehb.be>
2025-06-17 15:22:46 +0200

    login
    login blade + controller + afb

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/login', fn() => view('login.index'))->name('login');
+Route::get('/register', fn() => view('register.index'))->name('register');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::post('/login', [LoginController::class, 'login'])->name('login.attempt');
 
-require __DIR__.'/auth.php';
+Route::post('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/login');
+})->name('logout');
