ccdff097ba94fc96b97a00c0aea117173a90f21c
NoahGoethals <noah.goethals@student.ehb.be>
2025-06-17 15:08:04 +0200

    Mappen
    Mappen gemaakt en migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,29 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    
     public function run(): void
     {
-        // User::factory(10)->create();
+        \DB::statement('PRAGMA foreign_keys = OFF;');
+        \DB::table('users')->delete();
+        \DB::table('products')->delete();
+        \DB::statement('PRAGMA foreign_keys = ON;');
+
 
         User::factory()->create([
             'name' => 'Test User',
+            'username' => 'testuser',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => false,
         ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'admin@aquafin.be',
+            'password' => bcrypt('admin123'),
+            'is_admin' => true,
+        ]);
+        $this->call(\Database\Seeders\ProductSeeder::class);
     }
 }
