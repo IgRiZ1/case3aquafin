d4b5bdc3df6c560442d947b78048b78ee7fe44b4
Guerrero2103 <abdullahguerrero111@gmail.com>
2025-06-18 09:52:59 +0200

    Routes aangepast

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,87 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AdminProductController;
+use App\Http\Controllers\AdminOrderController;
+
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+Route::get('/login', function () {
+    return view('login.index');
+});
+
+Route::get('/register', function () {
+    return view('register.index');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/startpagina', function () {
+        return view('startpagina.index');
+    });
+    // Admin routes
+    Route::get('/admin/producten', function (\Illuminate\Http\Request $request) {
+        if (!auth()->user() || !auth()->user()->is_admin) {
+            abort(403);
+        }
+        return app(\App\Http\Controllers\AdminProductController::class)->index($request);
+    })->name('admin.producten.index');
+    Route::get('/admin/producten/{id}/edit', function ($id) {
+        if (!auth()->user() || !auth()->user()->is_admin) {
+            abort(403);
+        }
+        return app(\App\Http\Controllers\AdminProductController::class)->edit($id);
+    })->name('admin.producten.edit');
+    Route::put('/admin/producten/{id}', function ($id) {
+        if (!auth()->user() || !auth()->user()->is_admin) {
+            abort(403);
+        }
+        return app(\App\Http\Controllers\AdminProductController::class)->update(request(), $id);
+    })->name('admin.producten.update');
+    Route::delete('/admin/producten/{id}', function ($id) {
+        if (!auth()->user() || !auth()->user()->is_admin) {
+            abort(403);
+        }
+        return app(\App\Http\Controllers\AdminProductController::class)->destroy($id);
+    })->name('admin.producten.destroy');
+    Route::get('/admin/producten/create', function () {
+        if (!auth()->user() || !auth()->user()->is_admin) {
+            abort(403);
+        }
+        return app(\App\Http\Controllers\AdminProductController::class)->create();
+    })->name('admin.producten.create');
+    Route::post('/admin/producten', function () {
+        if (!auth()->user() || !auth()->user()->is_admin) {
+            abort(403);
+        }
+        return app(\App\Http\Controllers\AdminProductController::class)->store(request());
+    })->name('admin.producten.store');
+    Route::get('/admin/orders', [\App\Http\Controllers\AdminOrderController::class, 'index'])->name('admin.orders.index');
+    Route::get('/admin/orders/{id}', [\App\Http\Controllers\AdminOrderController::class, 'show'])->name('admin.orders.show');
+    Route::delete('/admin/orders/{id}', [\App\Http\Controllers\AdminOrderController::class, 'destroy'])->name('admin.orders.destroy');
+});
 
 Route::get('/producten', [ProductController::class, 'index'])->name('producten.index');
+
 Route::get('/producten/{id}', [ProductController::class, 'show'])->name('producten.show');
 
-Route::get('/login', fn() => view('login.index'))->name('login');
-Route::get('/register', fn() => view('register.index'))->name('register');
+Route::get('/winkelmandje', function () {
+    return view('producten.cart');
+});
+
+Route::post('/cart/add/{product}', [CartController::class, 'addToCart'])->name('cart.add');
+Route::get('/cart', [CartController::class, 'showCart'])->name('cart.show');
+Route::delete('/cart/remove/{id}', [CartController::class, 'removeFromCart'])->name('cart.remove');
+
+Route::post('/cart/order', [CartController::class, 'order'])->name('cart.order');
+Route::get('/cart/confirmation', function () {
+    return view('producten.confirmation');
+})->name('cart.confirmation');
 
 Route::post('/login', [LoginController::class, 'login'])->name('login.attempt');
 
@@ -18,9 +91,3 @@ Route::post('/logout', function () {
     request()->session()->regenerateToken();
     return redirect('/login');
 })->name('logout');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/startpagina', function () {
-        return view('startpagina.index');
-    })->name('startpagina');
-});
